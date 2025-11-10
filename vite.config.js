4f2012979f40b1816cf3cea40aa90ced6281970e
pranayy1 <pranayy7024@gmail.com>
2025-11-10 18:41:17 +0530

    fix the base parameter in config file

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/StudyHub/',
+    base: '/studyhub/',
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
